906c7c0620ff5c34b7d88cb4055338441106d588
Sanny26 <santhoshini26@gmail.com>
2018-09-18 16:09:28 +0530

    version 1

--- retrieval/views.py
+++ retrieval/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect, HttpResponse, reverse
 from django.core.paginator import Paginator
-# from django.utils.httpwrappers import 
 
 import numpy as np
 import cv2
-import base64
-from io import BytesIO, StringIO
 from PIL import Image
+import pickle
+import matplotlib.pyplot as plt
 
 from .models import Collections
 from .forms import SelectForm, UploadForm
@@ -36,6 +35,7 @@ def upload_query(request):
 	model_path = 'saved_models/new-iam.t7'
 	kdtree_path = 'saved_models/mohanlal_kdtree.p'
 	page2word_path = 'saved_models/page_to_word.p'
+	wrd_pos_fpath = 'saved_models/positions.pkl'
 	
 	context = {}
 	chosen_id = request.session['chosen_id']
@@ -52,10 +52,21 @@ def upload_query(request):
 			page2word = open(page2word_path, 'rb')
 
 			results = query_word(img_feat, kdtree, page2word) 
-			request.session['results'] = results[0]
 
 			request.session['qimg'] = img.tolist()
-			return redirect('results')
+
+			with open(wrd_pos_fpath, 'rb') as fobj:
+				wrd_pos = pickle.load(fobj)
+
+			positions = []
+			for each in results[0]:
+				pos = [int(pos) for pos in wrd_pos[each]]
+				positions.append(pos)
+
+			request.session['results'] = results[0]
+			request.session['pos'] = positions
+			#return redirect('results')
+			return redirect(reverse('view_results', args=[0]))
 	else:
 		form = UploadForm()
 
@@ -66,8 +77,14 @@ def upload_query(request):
 
 def show_image(request):
 	qimg = np.array(request.session['qimg'])
-	
-	print(qimg.dtype, qimg.shape)
+	qimg = Image.fromarray(np.uint8(qimg))
+	response = HttpResponse(content_type="image/png")
+	qimg.save(response, "PNG")
+	return response
+
+
+def show_page(request):
+	qimg = np.array(request.session['nimg'])
 	qimg = Image.fromarray(np.uint8(qimg))
 	response = HttpResponse(content_type="image/png")
 	qimg.save(response, "PNG")
@@ -76,19 +93,62 @@ def show_image(request):
 
 def results(request):
 	page_template = 'retrieval/results.html'
+	
 	results = request.session['results']
 	collection_id = request.session['chosen_id']
+	pos = request.session['pos']
 
 	context = {}
 	pages = []
+	page_ims = []
 	for each in results:
+		nimg_path = "media/cleaned/"+each.split('/')[0]+'.jpg'
+		nimg = cv2.imread(nimg_path)
+		y1, y2, x1, x2 = pos
+		print(each, pos)
+		nimg = cv2.rectangle(nimg, (x1, y1), (x2, y2), (0, 255, 0), 3)
+		#cv2.imwrite('test.jpg', nimg)
+		#pages.append(each.split('/')[0]+'.jpg')
+		page_ims.append(nimg.tolist())
 		pages.append(each.split('/')[0]+'.jpg')
+	
 	paginator = Paginator(pages, 1)
 
 	display_page = request.GET.get('page')
 	pages = paginator.get_page(display_page)
 
+	#request.session['page_ims'] = pages_ims
 	context['pages'] = pages
 	context['qimg'] = reverse('show_image')
-
 	return render(request, page_template, context) 
+
+
+def view_results(request, pid):
+	page_template = 'retrieval/view_results.html'
+	
+	pid = int(pid)
+	results = request.session['results']
+	collection_id = request.session['chosen_id']
+	positions = request.session['pos']
+
+	context = {}
+	path = results[pid]
+	pos = positions[pid]
+	nimg_path = "media/cleaned/"+path.split('/')[0]+'.jpg'
+	nimg = cv2.imread(nimg_path)
+	y1, y2, x1, x2 = pos
+	nimg = cv2.rectangle(nimg, (x1, y1), (x2, y2), (0, 255, 0), 3)
+	context['qimg'] = reverse('show_image')
+	request.session['nimg'] = nimg.tolist()
+	context['nimg'] = reverse('show_page')
+
+	context['pid'] = pid
+	if pid!=(len(pos)-1):
+		context['next_pid'] = pid+1
+	else:
+		context['next_pid'] = -1
+	if pid>0:
+		context['prev_pid'] = int(pid)-1
+	else:
+		context['prev_pid'] = -1
+	return render(request, page_template, context)
