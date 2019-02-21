7e7d3484c1d18bd94f1d7c5ad8eb7f234878da82
Sanny26 <santhoshini.g14@iiits.in>
2019-02-22 03:11:07 +0530

    To add: word-cloud, image optimization

--- website/search/urls.py
+++ website/search/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='home'),
-	path('results', views.results, name='results'),
+	path('results', views.line_results, name='results'),
 	path('show_image', views.show_image, name='show_image'),
 	path('show_page', views.show_page, name='show_page'),
 	path('project', views.about_project, name='about_project'),
+	re_path(r'^show_line/(?P<pid>\d)/$', views.show_line, name='show_line'),
 	re_path(r'^search/(?P<cname>\w+)/$', views.query, name='search'),
 	re_path(r'^chome/(?P<cname>\w+)/$', views.collection_index, name='chome'),
 	re_path(r'^mresults/(?P<page>\d)/$', views.mresults, name='mresults'),
